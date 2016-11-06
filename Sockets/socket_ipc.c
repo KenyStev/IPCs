9549630b7e4006adca1d3f02a71df56afa2c4f7e
Raim <rai.interiano@gmail.com>
2016-11-06 16:38:06 -0600

    Something that was done; thread gets hung

--- Sockets/socket_ipc.c
+++ Sockets/socket_ipc.c
@@ -5,7 +5,7 @@ pid_t root;
 
 int main(int argc, char *argv[]){
   node_attr.val = atoi(argv[1]);
-  printf("My value is %d\n", node_attr.val);
+  // printf("My value is %d\n", node_attr.val);
   socket_serv();
   pthread_t pth;
   // pthread_create(&pth, NULL, (void*)socket_serv, NULL);
